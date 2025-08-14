d5a559a3bd96ff371927174a207c36a057bf9cce
Pinni Plevinski <shareytora007@gmail.com>
2025-08-14 14:23:25 +0300

    creating MYSQL pvc, deployment, service and expose it

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -2,5 +2,8 @@
 oc login --token=<API_TOKEN> --server=https://api.rm3.7wse.p1.openshiftapps.com:6443
 
 @REM --- MYSQL creation ---
-@REM create secret
-oc apply -f mysql-secret.yaml -n pplevins-dev
+oc apply -f mysql-secret.yaml -n pplevins-dev   @REM create secret
+oc apply -f mysql-pvc.yaml -n pplevins-dev      @REM create pvc
+oc apply -f mysql-deployment.yaml -n pplevins-dev @REM create deployment
+oc apply -f mysql-service.yaml -n pplevins-dev  @REM create service
+oc expose service mysql -n pplevins-dev         @REM exposing the mysql service
