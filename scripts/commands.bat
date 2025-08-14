69b4b8c86b43e5e101fc7a25a9e0e91d74f0b361
Pinni Plevinski <shareytora007@gmail.com>
2025-08-14 12:58:45 +0300

    Adding mysql-secret.yaml

--- scripts/commands.bat
+++ scripts/commands.bat
@@ -1,1 +1,6 @@
+@REM login to OpenShift, API_TOKEN removed for discretion
 oc login --token=<API_TOKEN> --server=https://api.rm3.7wse.p1.openshiftapps.com:6443
+
+@REM --- MYSQL creation ---
+@REM create secret
+oc apply -f mysql-secret.yaml -n pplevins-dev
