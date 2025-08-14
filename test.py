6db9ccacf949a2b59db4957150ffa824832e42de
Pinni Plevinski <shareytora007@gmail.com>
2025-08-15 02:44:03 +0300

    Adding note to the test file reminding to port-forward before use

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import uvicorn
 
 from services.data_loader.api_server import app
 
+# NOTE: to use this test file on your local computer, make sure to port-forward the MySql service
+# using the command: oc port-forward svc/mysql 3306:3306 -n pplevins-dev
+
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
